f155bb87bd0f11166fedc7e24edd11a95fd015bc
Shevchenko Oleg <shevchenko.oleg5@gmail.com>
2021-01-13 16:52:34 +0200

    Add socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
 // import {TypeOrmModule} from "@nestjs/typeorm";
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommandsModule } from './commands/commands.module';
+import { ChatsModule } from './chats/chats.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
       ConfigModule.forRoot({envFilePath:'.env', isGlobal: true}),
       // TypeOrmModule.forRoot(),
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client'),
+      }),
       CommandsModule,
+      ChatsModule,
+      RoomsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
